cdc3be581e1d4a8fa0ef23ef686921f417962b32
Aaron <iaaron.xyz@outlook.com>
2024-02-10 18:42:14 -0600

    base projects section

--- src/components/WorksPage.jsx
+++ src/components/WorksPage.jsx
@@ -1,5 +1,16 @@
 const WorksPage = () => {
-  return <>Projects Section</>;
+  return (
+    <>
+      <div className='content-width my-16 flex flex-col'>
+        <h1 className='text-3xl mb-4'>Projects</h1>
+        <img
+          src='https://placehold.co/600x400/orange/white'
+          alt='placeholder image'
+          className='rounded-lg'
+        />
+      </div>
+    </>
+  );
 };
 
 export default WorksPage;
