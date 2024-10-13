0c0fc732c91a786015c19077caa099edcdf0f4ed
Aaron <iaaron.xyz@outlook.com>
2024-10-13 02:29:19 -0600

    add styles depending on the team

--- src/scripts/themeToggle.js
+++ src/scripts/themeToggle.js
@@ -4,18 +4,22 @@ toggleButton.addEventListener("click", () => {
   if (localStorage.theme === "dark") {
     localStorage.theme = "light";
     document.documentElement.classList.remove("dark");
+    toggleButton.innerText = "M";
   } else {
     localStorage.theme = "dark";
     document.documentElement.classList.add("dark");
+    toggleButton.innerText = "S";
   }
 });
 
 // Set initial theme based on local storage or system preference
 if (localStorage.theme === "dark") {
   document.documentElement.classList.add("dark");
+  toggleButton.innerText = "S";
 } else if (
   !("theme" in localStorage) &&
   window.matchMedia("(prefers-color-scheme: dark)").matches
 ) {
   document.documentElement.classList.add("dark");
+  toggleButton.innerText = "S";
 }
