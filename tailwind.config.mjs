ca2638df17bb80371acd128dea7151676d2e333b
Aaron <iaaron.xyz@outlook.com>
2024-10-13 18:26:12 -0600

    create MyStack component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: 'class',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				'sentient-regular': ['Sentient-Regular', 'serif'],
-				'sentient-light': ['Sentient-Light' , 'serif'],
-				'inter-light': ['Inter-Light', 'sans'],
-				'inter-regular': ['Inter-Regular', 'sans'],
-				'inter-black': ['Inter-Black', 'sans']
-			}
-		},
-	},
-	plugins: [],
-}
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        night: "#0D1117",
+      },
+      fontFamily: {
+        "sentient-regular": ["Sentient-Regular", "serif"],
+        "sentient-light": ["Sentient-Light", "serif"],
+        "inter-light": ["Inter-Light", "sans"],
+        "inter-regular": ["Inter-Regular", "sans"],
+        "inter-black": ["Inter-Black", "sans"],
+      },
+    },
+  },
+  plugins: [],
+};
