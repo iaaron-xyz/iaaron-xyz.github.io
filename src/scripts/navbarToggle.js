2e3f61c5f1cf02962d680e19a3857ae52b4d0910
Aaron <iaaron.xyz@outlook.com>
2024-12-12 02:19:58 -0600

    Add toggle effect to navbar for small screens

---
+++ src/scripts/navbarToggle.js
@@ -0,0 +1,4 @@
+document.querySelector(".hamburger").addEventListener("click", () => {
+  //   document.querySelector("nav")?.classList.toggle("h-full");
+  document.querySelector(".navbar-content")?.classList.toggle("navbar-show");
+});
