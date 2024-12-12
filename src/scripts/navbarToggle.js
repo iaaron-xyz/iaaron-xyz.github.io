449c20d7a27bd9edb91ee4733096b885ec9f2375
Aaron <iaaron.xyz@outlook.com>
2024-12-12 02:55:39 -0600

    style navbar for small screens

--- src/scripts/navbarToggle.js
+++ src/scripts/navbarToggle.js
@@ -1,4 +1,3 @@
 document.querySelector(".hamburger").addEventListener("click", () => {
-  //   document.querySelector("nav")?.classList.toggle("h-full");
   document.querySelector(".navbar-content")?.classList.toggle("navbar-show");
 });
